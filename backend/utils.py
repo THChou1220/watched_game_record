4c5884a64738948270702abef239da3ab687fac8
THChou1220 <99878260+THChou1220@users.noreply.github.com>
2025-03-23 13:05:17 +0800

    Update utils.py

--- backend/utils.py
+++ backend/utils.py
@@ -3,8 +3,8 @@ import mysql.connector
 def get_db_conn():
     conn = mysql.connector.connect(
         host="127.0.0.1",
-        user="root",            # your MySQL username
-        password="cth20021220", # your MySQL password
+        user="",            # your MySQL username
+        password="", # your MySQL password
         database="viewed_game"
     )
     return conn
